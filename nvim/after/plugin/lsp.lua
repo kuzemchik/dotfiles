ce46dd0f0a1a02c385c179007c7a78f1ffcc0977
Vladislav Kuzemchik <vlad@kuzemchik.me>
2023-11-25 00:25:18 -0500

    small change

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -46,7 +46,7 @@ cmp.setup({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-s>"] = cmp.mapping.complete(),
         ['<Tab>'] = nil,
         ['<S-Tab>'] = nil,
     })
