92ad65a9be3d64213cfa37d078a3c00f9fdf32cf
Vladislav Kuzemchik <vlad@kuzemchik.me>
2023-12-11 14:32:06 -0500

    debugger

--- nvim/lua/nogod/packer.lua
+++ nvim/lua/nogod/packer.lua
@@ -27,6 +27,7 @@ return require('packer').startup(function(use)
     use('ThePrimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use 'mfussenegger/nvim-dap'
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
